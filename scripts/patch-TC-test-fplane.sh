a6e99b07142fe764507a8efcf72fa8e5a82ed5b9
Colin Zarzycki <zarzycki@ucar.edu>
2020-08-26 10:00:41 -0600

    add tau diags, LY namelist support

--- scripts/patch-TC-test-fplane.sh
+++ scripts/patch-TC-test-fplane.sh
@@ -6,12 +6,12 @@ shopt -s expand_aliases
 # to create a patch...
 if [ "1" -eq "0" ]; then
   CESMROOT=~/work/cesm-dev/
-  filename="shr_flux_mod"
-  SUBPATH="cime/src/share/util/"
-  CESMCOMPONENT="share"
-  SOURCEMODDIR=~/ideal-mods/CLUBB/SourceDiffs/src.${CESMCOMPONENT}/
+  filename="advance_clubb_core_module"
+  SUBPATH="components/cam/src/physics/clubb/"
+  CESMCOMPONENT="cam"
+  SOURCEMODDIR=~/ideal-mods/CLUBB/20200819_taudiags/SourceDiffs/src.${CESMCOMPONENT}/
   mkdir -p ${SOURCEMODDIR}
-  diff -b -U 3 ${CESMROOT}/${SUBPATH}/${filename}.F90 ~/TC_PBL/RCE.QPC6.ne0np4tcfplane.ne15x8.exp297.001/SourceMods/src.${CESMCOMPONENT}/${filename}.F90 > ${SOURCEMODDIR}/${filename}.patch
+  diff -b -U 3 ${CESMROOT}/${SUBPATH}/${filename}.F90 ~/TC_PBL/RCE.QPC6.ne0np4tcfplane.ne15x8.exp294.001/SourceMods/src.${CESMCOMPONENT}/${filename}.F90 > ${SOURCEMODDIR}/${filename}.patch
 fi
 
 function patch_code {
@@ -20,7 +20,7 @@ function patch_code {
   local filename=${2}
   local SUBPATH=${3}
   local CESMCOMPONENT=${4}
-  echo "Patching ${filename}.F90..."
+  echo "Patching ${filename}.F90... using patch file from ${PATCHDIR}"
   cp ${CESMROOT}/${SUBPATH}/${filename}.F90 ./SourceMods/src.${CESMCOMPONENT}/
   patch ${PATCHFLAGS} ./SourceMods/src.${CESMCOMPONENT}/${filename}.F90 < ${PATCHDIR}/src.${CESMCOMPONENT}/${filename}.patch
   echo "----------------------------"
@@ -29,7 +29,7 @@ function patch_code {
 DTIME=450.0
 RES="ne15x8"
 PHYS="QPC6"  # QPC5 or QPC6
-EXPID="exp296"
+EXPID="exp293"
 ENS="001"
 NLEV=56
 NDAYS=10
@@ -44,7 +44,8 @@ CK10=9999.9
 ## CAM5 settings
 CAM5_DO_MG2=true
 ## General settings
-CD_CAP_MS=31.0         # negative means no cap
+LY09=true             # Do LY09?
+CD_CAP_MS=31.0        # negative means no changes
 
 if [[ $(hostname -s) = cheyenne* ]]; then
   IDEALDIR=/glade/u/home/zarzycki/ideal-mods/
@@ -55,7 +56,7 @@ if [[ $(hostname -s) = cheyenne* ]]; then
   CESMROOT=/glade/u/home/zarzycki/work/cesm-dev/
   GRIDSDIR=~/work/grids/
   INICDIR=~/work/initial/tc_testcase/
-  CSMDATA=/glade/p/cesmdata/cseg/
+  CSMDATA=/glade/p/cesmdata/cseg/inputdata/
   RCEINPUTDATA=/glade/u/home/kareed/RCE_inputdata/
   CASEDIR=~/TC_PBL/
   MACHNAME="cheyenne"
@@ -119,21 +120,24 @@ PATCHDIR=${IDEALDIR}/RCE/SourceDiffs/
 #patch_code ${PATCHDIR} "radiation" "components/cam/src/physics/rrtmg/" "cam"
 patch_code ${PATCHDIR} "cube_mod" "components/cam/src/dynamics/se/dycore/" "cam"
 #patch_code ${PATCHDIR} "docn_comp_mod" "cime/src/components/data_comps/docn/mct/" "docn"
-patch_code ${PATCHDIR} "shr_flux_mod" "cime/src/share/util/" "share"
 
-if (( $(echo "$CD_CAP_MS > 0" | bc -l) )); then
-  echo "Adjusting Cd"
-  cd ./SourceMods/src.share/
-    sed -i -e 's/33.0000/'${CD_CAP_MS}'/g' shr_flux_mod.F90
-  cd ${CASEDIR}/${CASENAME}
+if [ "${LY09}" = true ] ; then
+  echo "Using Large and Yeager Cd modifications"
+  patch_code ${PATCHDIR} "shr_flux_mod" "cime/src/share/util/" "share"
+  if (( $(echo "$CD_CAP_MS > 0" | bc -l) )); then
+    echo "Adjusting Cd"
+    cd ./SourceMods/src.share/
+      sed -i -e 's/33.0000/'${CD_CAP_MS}'/g' shr_flux_mod.F90
+    cd ${CASEDIR}/${CASENAME}
+  fi
 fi
 
 ## CMZ: min wind now controlled by  seq_flux_atmocn_minwind = 1.0D0
 if [ ${PHYS} == 'QPC6' ] ; then
-  PATCHDIR=${IDEALDIR}/CLUBB/SourceDiffs/
-  #patch_code ${PATCHDIR} "advance_clubb_core_module" "components/cam/src/physics/clubb/" "cam"
-  #patch_code ${PATCHDIR} "clubb_api_module" "components/cam/src/physics/clubb/" "cam"
-  #patch_code ${PATCHDIR} "clubb_intr" "components/cam/src/physics/cam/" "cam"
+  PATCHDIR=${IDEALDIR}/CLUBB/20200819_taudiags/SourceDiffs/
+  patch_code ${PATCHDIR} "advance_clubb_core_module" "components/cam/src/physics/clubb/" "cam"
+  patch_code ${PATCHDIR} "stats_variables" "components/cam/src/physics/clubb/" "cam"
+  patch_code ${PATCHDIR} "stats_zm_module" "components/cam/src/physics/clubb/" "cam"
   #cp /glade/u/home/zarzycki/ideal-mods/CLUBB/tau-sculpt/vince-modif/*F90 ./SourceMods/src.cam/
   cd ./SourceMods/src.cam/
   
@@ -185,7 +189,7 @@ elif [ ${PHYS} == 'QPC6' ]; then
   # Change to Kevin's RCE use case
   ./xmlchange CAM_NML_USE_CASE="aquaplanet_rce_cam6"
    # Add CAM6 specific output vars
-  EXTRAVARS="'Lscale','wp2','C11_Skw_fnc'"
+  EXTRAVARS="'Lscale','em','Kh_zm','Kh_zt','invrs_tau_bkgnd','invrs_tau_sfc','invrs_tau_shear','bv_freq_sqd'"
   DIAGOUT=","$EXTRAVARS
 else
   DIAGOUT=""
@@ -253,7 +257,7 @@ clubb_l_predict_upwp_vpwp = .${PREDICT_UPWP}.
 clubb_l_diag_lscale_from_tau = .${DIAG_LSCALE_FROM_TAU}.
 clubb_history=.true.
 clubb_vars_zt = 'thlm', 'thvm', 'rtm', 'rcm', 'rvm', 'um', 'vm', 'um_ref','vm_ref','ug', 'vg', 'cloud_frac', 'cloud_cover', 'rcm_in_layer', 'rcm_in_cloud', 'p_in_Pa', 'exner', 'rho_ds_zt', 'thv_ds_zt', 'Lscale', 'Lscale_pert_1', 'Lscale_pert_2', 'T_in_K', 'rel_humidity', 'wp3', 'wpthlp2', 'wp2thlp', 'wprtp2', 'wp2rtp', 'Lscale_up', 'Lscale_down', 'tau_zt', 'Kh_zt', 'wp2thvp', 'wp2rcp', 'wprtpthlp', 'sigma_sqd_w_zt', 'rho', 'radht', 'radht_LW', 'radht_SW', 'Ncm', 'Nc_in_cloud', 'Nc_activated', 'snowslope', 'sed_rcm', 'rsat', 'rsati', 'diam', 'mass_ice_cryst', 'rcm_icedfs', 'u_T_cm', 'rtm_bt', 'rtm_ma', 'rtm_ta', 'rtm_mfl', 'rtm_tacl', 'rtm_cl', 'rtm_forcing', 'rtm_sdmp','rtm_mc', 'rtm_pd', 'rvm_mc', 'rcm_mc', 'rcm_sd_mg_morr', 'thlm_bt', 'thlm_ma', 'thlm_ta', 'thlm_mfl', 'thlm_tacl', 'thlm_cl', 'thlm_forcing', 'thlm_sdmp','thlm_mc', 'thlm_old', 'thlm_without_ta', 'thlm_mfl_min', 'thlm_mfl_max', 'thlm_enter_mfl', 'thlm_exit_mfl', 'rtm_old', 'rtm_without_ta', 'rtm_mfl_min', 'rtm_mfl_max', 'rtm_enter_mfl', 'rtm_exit_mfl', 'um_bt', 'um_ma', 'um_gf', 'um_cf', 'um_ta', 'um_f', 'um_sdmp', 'um_ndg', 'vm_bt', 'vm_ma', 'vm_gf', 'vm_cf', 'vm_ta', 'vm_f', 'vm_sdmp', 'vm_ndg', 'wp3_bt', 'wp3_ma', 'wp3_ta', 'wp3_tp', 'wp3_ac', 'wp3_bp1', 'wp3_bp2', 'wp3_pr1', 'wp3_pr2', 'wp3_dp1', 'wp3_cl', 'mixt_frac', 'w_1', 'w_2', 'varnce_w_1', 'varnce_w_2', 'thl_1', 'thl_2', 'varnce_thl_1', 'varnce_thl_2', 'rt_1', 'rt_2', 'varnce_rt_1', 'varnce_rt_2', 'rc_1', 'rc_2', 'rsatl_1', 'rsatl_2', 'cloud_frac_1', 'cloud_frac_2', 'a3_coef_zt', 'wp3_on_wp2_zt', 'chi_1', 'chi_2', 'stdev_chi_1', 'stdev_chi_2', 'stdev_eta_1', 'stdev_eta_2', 'covar_chi_eta_1', 'covar_chi_eta_2', 'corr_chi_eta_1', 'corr_chi_eta_2', 'corr_rt_thl_1', 'crt_1', 'crt_2', 'cthl_1', 'cthl_2', 'precip_frac', 'precip_frac_1', 'precip_frac_2', 'Ncnm', 'wp2_zt', 'thlp2_zt', 'wpthlp_zt', 'wprtp_zt', 'rtp2_zt', 'rtpthlp_zt', 'up2_zt', 'vp2_zt', 'upwp_zt', 'vpwp_zt', 'C11_Skw_fnc'
-clubb_vars_zm = 'wp2', 'rtp2', 'thlp2', 'rtpthlp', 'wprtp', 'wpthlp', 'wp4', 'up2', 'vp2', 'wpthvp', 'rtpthvp', 'thlpthvp', 'tau_zm', 'Kh_zm', 'wprcp', 'wm_zm', 'thlprcp', 'rtprcp', 'rcp2', 'upwp', 'vpwp', 'rho_zm', 'sigma_sqd_w', 'Skw_velocity', 'gamma_Skw_fnc', 'C6rt_Skw_fnc', 'C6thl_Skw_fnc', 'C7_Skw_fnc', 'C1_Skw_fnc', 'a3_coef', 'wp3_on_wp2', 'rcm_zm', 'rtm_zm', 'thlm_zm', 'cloud_frac_zm', 'rho_ds_zm', 'thv_ds_zm', 'em', 'mean_w_up', 'mean_w_down', 'shear', 'wp3_zm', 'Frad', 'Frad_LW', 'Frad_SW', 'Frad_LW_up', 'Frad_SW_up', 'Frad_LW_down', 'Frad_SW_down', 'Fprec', 'Fcsed', 'wp2_bt', 'wp2_ma', 'wp2_ta', 'wp2_ac', 'wp2_bp', 'wp2_pr1', 'wp2_pr2', 'wp2_pr3', 'wp2_dp1', 'wp2_dp2', 'wp2_cl', 'wp2_pd', 'wp2_sf', 'vp2_bt', 'vp2_ma', 'vp2_ta', 'vp2_tp', 'vp2_dp1', 'vp2_dp2', 'vp2_pr1', 'vp2_pr2', 'vp2_cl', 'vp2_pd', 'vp2_sf', 'up2_bt', 'up2_ma', 'up2_ta', 'up2_tp', 'up2_dp1', 'up2_dp2', 'up2_pr1', 'up2_pr2', 'up2_cl', 'up2_pd', 'up2_sf', 'wprtp_bt', 'wprtp_ma', 'wprtp_ta', 'wprtp_tp', 'wprtp_ac', 'wprtp_bp', 'wprtp_pr1', 'wprtp_pr2', 'wprtp_pr3', 'wprtp_dp1', 'wprtp_mfl', 'wprtp_cl', 'wprtp_sicl', 'wprtp_pd', 'wprtp_forcing', 'wprtp_mc', 'wpthlp_bt', 'wpthlp_ma', 'wpthlp_ta', 'wpthlp_tp', 'wpthlp_ac', 'wpthlp_bp', 'wpthlp_pr1', 'wpthlp_pr2', 'wpthlp_pr3', 'wpthlp_dp1', 'wpthlp_mfl', 'wpthlp_cl', 'wpthlp_sicl', 'wpthlp_forcing', 'wpthlp_mc', 'rtp2_bt', 'rtp2_ma', 'rtp2_ta', 'rtp2_tp', 'rtp2_dp1', 'rtp2_dp2', 'rtp2_cl', 'rtp2_pd', 'rtp2_sf', 'rtp2_forcing', 'rtp2_mc', 'thlp2_bt', 'thlp2_ma', 'thlp2_ta', 'thlp2_tp', 'thlp2_dp1', 'thlp2_dp2', 'thlp2_cl', 'thlp2_pd', 'thlp2_sf', 'thlp2_forcing', 'thlp2_mc', 'rtpthlp_bt', 'rtpthlp_ma', 'rtpthlp_ta', 'rtpthlp_tp1', 'rtpthlp_tp2', 'rtpthlp_dp1', 'rtpthlp_dp2', 'rtpthlp_cl', 'rtpthlp_sf', 'rtpthlp_forcing', 'rtpthlp_mc', 'wpthlp_entermfl', 'wpthlp_exit_mfl', 'wprtp_enter_mfl', 'wprtp_exit_mfl', 'wpthlp_mfl_min', 'wpthlp_mfl_max', 'wprtp_mfl_min', 'wprtp_mfl_max', 'Richardson_num', 'shear_sqd'
+clubb_vars_zm = 'wp2', 'rtp2', 'thlp2', 'rtpthlp', 'wprtp', 'wpthlp', 'wp4', 'up2', 'vp2', 'wpthvp', 'rtpthvp', 'thlpthvp', 'tau_zm', 'Kh_zm', 'wprcp', 'wm_zm', 'thlprcp', 'rtprcp', 'rcp2', 'upwp', 'vpwp', 'rho_zm', 'sigma_sqd_w', 'Skw_velocity', 'gamma_Skw_fnc', 'C6rt_Skw_fnc', 'C6thl_Skw_fnc', 'C7_Skw_fnc', 'C1_Skw_fnc', 'a3_coef', 'wp3_on_wp2', 'rcm_zm', 'rtm_zm', 'thlm_zm', 'cloud_frac_zm', 'rho_ds_zm', 'thv_ds_zm', 'em', 'mean_w_up', 'mean_w_down', 'shear', 'wp3_zm', 'Frad', 'Frad_LW', 'Frad_SW', 'Frad_LW_up', 'Frad_SW_up', 'Frad_LW_down', 'Frad_SW_down', 'Fprec', 'Fcsed', 'wp2_bt', 'wp2_ma', 'wp2_ta', 'wp2_ac', 'wp2_bp', 'wp2_pr1', 'wp2_pr2', 'wp2_pr3', 'wp2_dp1', 'wp2_dp2', 'wp2_cl', 'wp2_pd', 'wp2_sf', 'vp2_bt', 'vp2_ma', 'vp2_ta', 'vp2_tp', 'vp2_dp1', 'vp2_dp2', 'vp2_pr1', 'vp2_pr2', 'vp2_cl', 'vp2_pd', 'vp2_sf', 'up2_bt', 'up2_ma', 'up2_ta', 'up2_tp', 'up2_dp1', 'up2_dp2', 'up2_pr1', 'up2_pr2', 'up2_cl', 'up2_pd', 'up2_sf', 'wprtp_bt', 'wprtp_ma', 'wprtp_ta', 'wprtp_tp', 'wprtp_ac', 'wprtp_bp', 'wprtp_pr1', 'wprtp_pr2', 'wprtp_pr3', 'wprtp_dp1', 'wprtp_mfl', 'wprtp_cl', 'wprtp_sicl', 'wprtp_pd', 'wprtp_forcing', 'wprtp_mc', 'wpthlp_bt', 'wpthlp_ma', 'wpthlp_ta', 'wpthlp_tp', 'wpthlp_ac', 'wpthlp_bp', 'wpthlp_pr1', 'wpthlp_pr2', 'wpthlp_pr3', 'wpthlp_dp1', 'wpthlp_mfl', 'wpthlp_cl', 'wpthlp_sicl', 'wpthlp_forcing', 'wpthlp_mc', 'rtp2_bt', 'rtp2_ma', 'rtp2_ta', 'rtp2_tp', 'rtp2_dp1', 'rtp2_dp2', 'rtp2_cl', 'rtp2_pd', 'rtp2_sf', 'rtp2_forcing', 'rtp2_mc', 'thlp2_bt', 'thlp2_ma', 'thlp2_ta', 'thlp2_tp', 'thlp2_dp1', 'thlp2_dp2', 'thlp2_cl', 'thlp2_pd', 'thlp2_sf', 'thlp2_forcing', 'thlp2_mc', 'rtpthlp_bt', 'rtpthlp_ma', 'rtpthlp_ta', 'rtpthlp_tp1', 'rtpthlp_tp2', 'rtpthlp_dp1', 'rtpthlp_dp2', 'rtpthlp_cl', 'rtpthlp_sf', 'rtpthlp_forcing', 'rtpthlp_mc', 'wpthlp_entermfl', 'wpthlp_exit_mfl', 'wprtp_enter_mfl', 'wprtp_exit_mfl', 'wpthlp_mfl_min', 'wpthlp_mfl_max', 'wprtp_mfl_min', 'wprtp_mfl_max', 'Richardson_num', 'shear_sqd','invrs_tau_bkgnd','invrs_tau_sfc','invrs_tau_shear','bv_freq_sqd'
 EOF
 else
   echo "nothing"
